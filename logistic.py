e1eeeb732c418866d52c2388bb83d81d5c772f83
Carlos Alcala <alcala21@gmail.com>
2022-07-23 15:49:54 -0500

    Stage 2/4. Gradient descent with MSE.

--- logistic.py
+++ logistic.py
@@ -1,9 +1,9 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
-# write your code here
 class CustomLogisticRegression:
 
     def __init__(self, fit_intercept=True, l_rate=0.01, n_epoch=100):
@@ -16,15 +16,31 @@ class CustomLogisticRegression:
 
     def predict_proba(self, row, coef_):
         if self.fit_intercept:
-            t = coef_[0] + row.dot(coef_[1:])
-        else:
-            t = row.dot(coef_)
+            row = np.insert(row, 0, np.ones(row.shape[0]), axis=1)
+        t = row.dot(coef_)
         return self.sigmoid(t)
+
+    def fit_mse(self, X_train, y_train):
+        n = X_train.shape[0]
+        if self.fit_intercept:
+            X_train = np.insert(X_train, 0, np.ones(n), axis=1)
+        self.coef_ = np.zeros(X_train.shape[1])
+        self.fit_intercept = False
+
+        for _ in range(self.n_epoch):
+            yhat = self.predict_proba(X_train, self.coef_)
+            dJdt = (yhat - y_train) * yhat * (1 - yhat)
+            self.coef_ += -self.l_rate * dJdt.dot(X_train)
+        self.fit_intercept = True
+
+    def predict(self, X_test, cut_off=0.5):
+        yhat = self.predict_proba(X_test, self.coef_)
+        return (yhat >= cut_off).astype(int)
 
 
 def load_data():
     data = datasets.load_breast_cancer()
-    col_names = ['worst concave points', 'worst perimeter']#
+    col_names = ['worst concave points', 'worst perimeter', 'worst radius']#
     col_inds = [np.where(data.feature_names == name)[0][0] for name in col_names]
     X = data.data[:,col_inds]
     y = data.target
@@ -45,9 +61,10 @@ X, y = load_data()
 X = standardise_data(X)
 X_train, X_test, y_train, y_test = split_data(X, y)
 
-coeffs = np.array([0.77001597, -2.12842434, -2.39305793])
 
-model = CustomLogisticRegression()
-
-print(list(model.predict_proba(X_test[:10], coeffs)))
-
+model = CustomLogisticRegression(fit_intercept=True, l_rate=0.01, n_epoch=1000)
+model.fit_mse(X_train, y_train)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_pred, y_test)
+res = {'coef_': list(model.coef_), 'accuracy': acc}
+print(res)
